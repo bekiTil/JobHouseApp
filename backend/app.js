055e402788fe73e9eab481e2420c5a82e76fdaa5
Bereket_Tilahun <btilahun287@gmail.com>
2022-05-01 20:30:40 +0300

    authorization and index update

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,13 @@ const bodyParser = require("body-parser");
 dotenv.config();
 
 // Connect to db
-mongoose.connect(process.env.DB, () => console.log("Connected to db!"));
+mongoose.connect(process.env['DATABASE'])
+.then(()=>console.log("connected to mongo db..."))
+.catch((err)=>console.log("Cannot connect to mongodb...", err)); 
 
 // Import Routes
-const authRoute = require("./routes/auth");
+const users = require('./routes/users')
+const auth = require('./routes/auth')
 const postRoute = require("./routes/post");
 
 const app = express();
@@ -18,7 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route Middleware
-app.use("", authRoute);
+app.use("/api/users", users);
+app.use('/login', auth)
 app.use("/images", express.static("images"));
 app.use("/post", postRoute);
 
