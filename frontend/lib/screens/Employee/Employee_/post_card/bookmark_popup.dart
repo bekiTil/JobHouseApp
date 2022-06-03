3aa9483d59749547dbd645fba11597fa1e6995f8
duressa <duressajemal3@gmail.com>
2022-06-03 23:55:10 +0300

    incorporated the popup bookmark add

--- frontend/lib/screens/Employee/Employee_/post_card/bookmark_popup.dart
+++ frontend/lib/screens/Employee/Employee_/post_card/bookmark_popup.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/blocs/bookmark/bookmark_bloc.dart';
+import 'package:frontend/models/models.dart';
 import 'package:frontend/models/post.dart';
+import 'package:frontend/repository/secureStorage.dart';
+import 'package:go_router/go_router.dart';
 
-void popBookmark(BuildContext context, Post post) async {
+void popBookmark(BuildContext context, Post post, id) async {
   final _formKey = GlobalKey<FormState>();
-
+  TextEditingController memoController = TextEditingController();
   await showDialog(
     context: context,
     barrierDismissible: true,
@@ -13,6 +18,7 @@ void popBookmark(BuildContext context, Post post) async {
         content: Form(
           key: _formKey,
           child: TextFormField(
+            controller: memoController,
             decoration: const InputDecoration(
               labelText: 'Memo *',
             ),
@@ -20,7 +26,7 @@ void popBookmark(BuildContext context, Post post) async {
               if (value == null || value.isEmpty) {
                 return 'Memo cannot be empty';
               }
-              //TODO: Send the bookmark request
+
               return null;
             },
           ),
@@ -29,9 +35,19 @@ void popBookmark(BuildContext context, Post post) async {
         actions: [
           IconButton(
             onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                Navigator.of(context, rootNavigator: true).pop();
-              }
+              if (_formKey.currentState!.validate()) {}
+              Bookmark bookmark = Bookmark(
+                  post: post,
+                  memo: memoController.value.text,
+                  post_id: post.id,
+                  user_id: id
+                  // createdAt: DateTime.now(),
+                  );
+              context
+                  .read<BookmarkBloc>()
+                  .add(BookmarkCreate(bookmark: bookmark));
+
+              Navigator.of(context).pop();
             },
             icon: const Icon(Icons.bookmark_add),
           )
--- frontend/lib/screens/Employee/Employee_/post_card/post_header.dart
+++ frontend/lib/screens/Employee/Employee_/post_card/post_header.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/models/post.dart';
+import 'package:frontend/repository/secureStorage.dart';
 
 import 'bookmark_popup.dart';
 
-class PostHeader extends StatelessWidget {
+class PostHeader extends StatefulWidget {
   final Post post;
   const PostHeader({Key? key, required this.post}) : super(key: key);
 
+  @override
+  State<PostHeader> createState() => _PostHeaderState();
+}
+
+class _PostHeaderState extends State<PostHeader> {
+   late final String? id;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    StorageService storage = StorageService();
+    getInfo() async {
+      id = await storage.getId();
+    }
+
+    super.initState();
+    getInfo();
+  }
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -34,7 +53,7 @@ class PostHeader extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    post.posterName ?? "Yeabsira Driba",
+                    widget.post.posterName ?? "Yeabsira Driba",
                     style: const TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.w500,
@@ -53,7 +72,7 @@ class PostHeader extends StatelessWidget {
           cursor: SystemMouseCursors.text,
           child: IconButton(
             onPressed: () {
-              popBookmark(context, post);
+              popBookmark(context, widget.post, id);
             },
             icon: const Icon(
               Icons.favorite_border_outlined,
