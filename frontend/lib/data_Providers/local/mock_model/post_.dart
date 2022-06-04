50912069e6dda79ad6f5c44fe18fa4030965b3b5
Bereket Tilahun <91558592+bekiTil@users.noreply.github.com>
2022-06-04 20:20:13 +0300

    Update post_.dart

--- frontend/lib/data_Providers/local/mock_model/post_.dart
+++ frontend/lib/data_Providers/local/mock_model/post_.dart
@@ -1,35 +1,35 @@
-// class MockPost {
-//   String id;
-//   int number;
-//   String description;
-//   String category;
+class MockPost {
+  String id;
+  int number;
+  String description;
+  String category;
 
-//   MockPost({
-//     required this.id,
-//     required this.number,
-//     required this.description,
-//     required this.category,
-//   });
+  MockPost({
+    required this.id,
+    required this.number,
+    required this.description,
+    required this.category,
+  });
 
-//   factory MockPost.fromJson(Map<String, dynamic> json) {
-//     return MockPost(
-//         id: json['_id'],
-//         number: json['number'],
-//         description: json['description'],
-//         category: json['category']);
-//   }
+  factory MockPost.fromJson(Map<String, dynamic> json) {
+    return MockPost(
+        id: json['_id'],
+        number: json['number'],
+        description: json['description'],
+        category: json['category']);
+  }
 
-//   Map<String, dynamic> toJson() => {
-//         "id": id,
-//         "number": number,
-//         "description": description,
-//         "category": category,
-//       };
-//   factory MockPost.fromApi(Map<String, dynamic> json) {
-//     return MockPost(
-//         id: json['post']['_id'],
-//         number: json['post']['number'],
-//         description: json['post']['description'],
-//         category: json['post']['category']);
-//   }
-// }
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "number": number,
+        "description": description,
+        "category": category,
+      };
+  factory MockPost.fromApi(Map<String, dynamic> json) {
+    return MockPost(
+        id: json['post']['_id'],
+        number: json['post']['number'],
+        description: json['post']['description'],
+        category: json['post']['category']);
+  }
+}
